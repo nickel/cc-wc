03cc2ba02b5a1652af95e1f423e7b6fe0259fc5a
Juan Gallego IV <juan.gallego.iv@gmail.com>
2026-01-05 00:01:22 +0000

    configuring CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Count bytes
+    #[arg(short = 'c', long)]
+    bytes: bool,
+
+    /// Count lines
+    #[arg(short = 'l', long)]
+    lines: bool,
+
+    /// Count words
+    #[arg(short = 'w', long)]
+    words: bool,
+
+    /// Count characters
+    #[arg(short = 'm', long)]
+    chars: bool,
+
+    /// File to process
+    file: String,
+}
+
 fn main() {
-  println!("Hello, World!");
+    let args = Args::parse();
+
+    println!("Hello {}!", args.file);
 }
